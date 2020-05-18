1708f0af764a5df68a25d0f9131fa4ab3519ea6c
Matthew Albrecht <matthewalbrecht@matthews-mbp.myfiosgateway.com>
2020-05-18 15:51:48 -0400

    reformate models index file

--- api/models/index.js
+++ api/models/index.js
@@ -12,44 +12,22 @@ const { UserAchievement } = require('./UserAchievement');
 const { UserAlbum } = require('./UserAlbum');
 const { UserAction } = require('./UserAction');
 
-// User.associate = (models) => {
 User.belongsToMany(Achievement, { through: UserAchievement, foreignKey: { name: 'userId' } });
 User.hasMany(List, { foreignKey: { name: 'userId' } });
 User.belongsToMany(Album, { through: UserAlbum, foreignKey: { name: 'userId' } });
 User.belongsToMany(Action, { through: UserAction, foreignKey: { name: 'userId' } });
-// };
 
-// Role.associate = (models) => {
 Role.hasMany(User, { foreignKey: 'role' });
 Role.belongsToMany(Permission, { through: RolePermission, foreignKey: { name: 'roleName' } });
-// };
 
-
-// Action.associate = (models) => {
 Action.belongsToMany(User, { through: UserAction, foreignKey: { name: 'actionCode' } });
-// };
-
-// Achievement.associate = (models) => {
 Achievement.belongsToMany(User, { through: UserAchievement, foreignKey: { name: 'achievementCode' } });
-// };
-
-// Album.associate = (models) => {
 Album.belongsToMany(User, { through: UserAlbum, foreignKey: { name: 'albumId' } });
-// };
-
-// List.associate = (models) => {
 List.belongsToMany(UserAlbum, { through: ListUserAlbum, foreignKey: { name: 'listId' } });
-// };
-
-// Permission.associate = (models) => {
 Permission.belongsToMany(Role, { through: RolePermission, foreignKey: { name: 'permissionName' } });
-// };
-
-// UserAlbum.associate = (models) => {
 UserAlbum.belongsToMany(List, { through: ListUserAlbum, foreignKey: { name: 'userAlbumId' } });
-// };
 
-const Models = {
+module.exports = {
   Achievement,
   Album,
   Action,
@@ -64,12 +42,3 @@ const Models = {
   UserAlbum,
   UserAction,
 };
-
-// Object.keys(Models).forEach((key) => {
-//   if ('associate' in Models[key]) {
-//     Models[key].associate(Models);
-//   }
-// });
-
-
-module.exports = Models;
